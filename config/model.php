c68d33d1f067f7f678c8959e4233ae819f79a00b
akbardhany <dhanydhuny@gmail.com>
2021-05-05 22:20:34 +0700

    Fungsi Read Peserta

--- config/model.php
+++ config/model.php
@@ -7,13 +7,28 @@
             $conn = $db->Connect();
 
             try {
-                
+
                 $createPeserta = "INSERT INTO Peserta (nama_peserta, jk_peserta, kategori_peserta) VALUES ('$namaPeserta', '$jkPeserta', '$kategoriPeserta')";
                 $conn->exec($createPeserta);
                 echo "<script>alert('Data Peserta $namaPeserta telah ditambahkan');window.location='../index.php'</script>";
 
             } catch (PDOException $e) {
                 echo $createPeserta."<br />".$e->getMessage();
+            }
+
+            $conn = $db->Close();
+        }
+
+        public function readPeserta(){
+            $db = new DB();
+            $conn = $db->Connect();
+
+            try {
+
+                $this->readPeserta = $conn->query("SELECT nama_peserta, jk_peserta, kategori_peserta FROM Peserta ORDER BY id_peserta DESC")->fetchAll();
+
+            } catch (PDOException $e) {
+                echo $this->readPeserta."<br />".$e->getMessage();
             }
 
             $conn = $db->Close();
