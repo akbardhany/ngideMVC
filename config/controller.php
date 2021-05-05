2a7f741739e9b6480cd6813a6b824aaf5e43a2b6
akbardhany <dhanydhuny@gmail.com>
2021-05-05 22:51:30 +0700

    Fungsi Menampilkan 1 Peserta

--- config/controller.php
+++ config/controller.php
@@ -9,5 +9,13 @@
                 $this->readPesertas[] = array($rowPeserta['id_peserta'], $rowPeserta['nama_peserta'], $rowPeserta['jk_peserta'], $rowPeserta['kategori_peserta']);
             }
         }
+
+        public function selectPeserta($idPeserta){
+            $mdl = new Model();
+            $mdl->selectedPeserta($idPeserta);
+            foreach ($mdl->selectedPeserta as $selectPeserta) {
+                $this->selectedPeserta[] = array($selectPeserta['id_peserta'], $selectPeserta['nama_peserta'], $selectPeserta['jk_peserta'], $selectPeserta['kategori_peserta']);
+            }
+        }
     }
 ?>
