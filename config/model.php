4bff3c60ae082f83839f11bb5409d19cae52a665
akbardhany <dhanydhuny@gmail.com>
2021-05-05 22:27:29 +0700

    Fungsi Selected Siswa

--- config/model.php
+++ config/model.php
@@ -25,10 +25,25 @@
 
             try {
 
-                $this->readPeserta = $conn->query("SELECT nama_peserta, jk_peserta, kategori_peserta FROM Peserta ORDER BY id_peserta DESC")->fetchAll();
+                $this->readPeserta = $conn->query("SELECT id_peserta, nama_peserta, jk_peserta, kategori_peserta FROM Peserta ORDER BY id_peserta DESC")->fetchAll();
 
             } catch (PDOException $e) {
                 echo $this->readPeserta."<br />".$e->getMessage();
+            }
+
+            $conn = $db->Close();
+        }
+
+        public function selectedPeserta($idPeserta){
+            $db = new DB();
+            $conn = $db->Connect();
+
+            try {
+
+                $this->selectedPeserta = $conn->query("SELECT id_peserta, nama_peserta, jk_peserta, kategori_peserta FROM Peserta WHERE id_peserta=$idPeserta")->fetchAll();
+
+            } catch (PDOException $e) {
+                echo $this->selectedPeserta."<br />".$e->getMessage();
             }
 
             $conn = $db->Close();
